e264e4609479bb2fbb68bdf01845ada9700d0eb1
cjsampe <samperiz@hotmail.com>
2021-04-20 18:37:17 +0100

    rama develop

--- src/main/java/com/sanvalero/libreria/Libreria.java
+++ src/main/java/com/sanvalero/libreria/Libreria.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sanvalero.libreria;
 
 /**
  *
- * @author Atramento
+ * @author Carmen Jiménez Sampériz DAW
  */
 public class Libreria {
     
