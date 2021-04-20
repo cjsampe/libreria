65a3f00e7d5c9a291d5b7bed74fe5cf9290899f9
cjsampe <samperiz@hotmail.com>
2021-04-20 18:51:48 +0100

    clases Libro, Musica y Pelicula

---
+++ src/main/java/com/sanvalero/libreria/domain/Musica.java
@@ -0,0 +1,59 @@
+/**
+ *
+ * @author Carmen Jiménez Sampériz
+ */
+public class Musica {
+    
+    private String nombreAlbum;
+    private String nombreGrupo;
+    private String discografica;
+    private float duracionAlbum;
+    private int anio;
+    
+    public Musica(){
+        
+    }
+
+    public String getNombreAlbum() {
+        return nombreAlbum;
+    }
+
+    public void setNombreAlbum(String nombreAlbum) {
+        this.nombreAlbum = nombreAlbum;
+    }
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public String getDiscografica() {
+        return discografica;
+    }
+
+    public void setDiscografica(String discografica) {
+        this.discografica = discografica;
+    }
+
+    public float getDuracionAlbum() {
+        return duracionAlbum;
+    }
+
+    public void setDuracionAlbum(float duracionAlbum) {
+        this.duracionAlbum = duracionAlbum;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
+    
+}
